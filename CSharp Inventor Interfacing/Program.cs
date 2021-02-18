5baa6396db5eef1c6a333e55ac8618f200b96c30
kctjohnson <58883551+kctjohnson@users.noreply.github.com>
2021-02-18 09:55:05 -0500

    Adding code to handle working on sub assembly iLogic files

--- CSharp Inventor Interfacing/Program.cs
+++ CSharp Inventor Interfacing/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Runtime.InteropServices;
 using Inventor;
@@ -24,6 +25,7 @@ namespace iLogic_Bridge {
     class Program {
         bool isOpen = false;
         static Program prog = new Program();
+        static Dictionary<string, dynamic> nameDocDict = new Dictionary<string, dynamic>();
         dynamic iLogicAuto;
         dynamic activeDoc;
 
@@ -84,10 +86,11 @@ namespace iLogic_Bridge {
                 prog.activeDoc = activeDoc;
 
                 Console.WriteLine("Creating iLogic Files In iLogicTransfer Folder...");
-                dynamic rules = prog.iLogicAuto.Rules(activeDoc);
-                foreach (dynamic r in rules) {
-                    System.IO.File.WriteAllText(iLogicTransferFolder + "\\" + r.Name + ".vb", r.text);
-                }
+                //dynamic rules = prog.iLogicAuto.Rules(activeDoc);
+                //foreach (dynamic r in rules) {
+                //    System.IO.File.WriteAllText(iLogicTransferFolder + "\\" + r.Name + ".vb", r.text);
+                //}
+                prog.SpanAssemblyTree(iLogicTransferFolder, activeDoc);
 
                 Console.WriteLine("Creating File Watcher...");
                 CreateFileWatcher(iLogicTransferFolder);
@@ -100,6 +103,58 @@ namespace iLogic_Bridge {
                 Console.WriteLine("Watching Files...");
 
                 Console.ReadLine();
+            }
+        }
+
+        public void SpanAssemblyTree(string mainPath, Document mainDoc) {
+            Console.WriteLine("Creating iLogic File Tree...");
+
+            dynamic rules = prog.iLogicAuto.Rules(mainDoc);
+            string assemblyName = mainDoc.DisplayName;
+            string curPath = mainPath + "\\" + assemblyName;
+
+            nameDocDict.Add(assemblyName, mainDoc);
+
+            Directory.CreateDirectory(curPath);
+            foreach (dynamic r in rules) {
+                System.IO.File.WriteAllText(curPath + "\\" + r.Name + ".vb", r.text);
+            }
+
+            // Traverse if it's an assembly
+            if (mainDoc.DocumentType == DocumentTypeEnum.kAssemblyDocumentObject) {
+                List<string> alreadyFound = new List<string>();
+                alreadyFound.Add(mainDoc.DisplayName);
+                IterateBranches(curPath, ((AssemblyDocument)mainDoc).ComponentDefinition.Occurrences, ref alreadyFound);
+            }
+        }
+
+        public void IterateBranches(string curPath, dynamic children, ref List<string> alreadyFound) {
+            foreach(ComponentOccurrence child in children) {
+                if (child.DefinitionDocumentType == DocumentTypeEnum.kAssemblyDocumentObject) {
+                    try {
+                        dynamic doc = child.Definition.Document;
+
+                        // Skip this one if it's been found before
+                        if (alreadyFound.Contains(doc.DisplayName)) {
+                            continue;
+                        }
+
+                        nameDocDict.Add(child._DisplayName, doc);
+                        alreadyFound.Add(doc.DisplayName);
+                        dynamic rules = prog.iLogicAuto.Rules(child.Definition.Document);
+                        string assemblyName = child._DisplayName;
+                        string newPath = curPath + "\\" + assemblyName;
+
+                        Directory.CreateDirectory(newPath);
+                        foreach(dynamic r in rules) {
+                            System.IO.File.WriteAllText(newPath + "\\" + r.name + ".vb", r.text);
+                        }
+
+                        IterateBranches(newPath, child.SubOccurrences, ref alreadyFound);
+                    } catch (Exception) {
+                        Console.WriteLine("Failed to enumerate {0}!", child.Name);
+                    }
+                }
             }
         }
 
@@ -172,41 +227,34 @@ namespace iLogic_Bridge {
         private static void OnRenamed(object source, RenamedEventArgs e) {
             Console.WriteLine("OnRenamed: {0} TO {1}", e.OldFullPath, e.FullPath);
 
-            string[] oldSplits = e.OldName.Split('\\');
-            string[] newSplits = e.Name.Split('\\');
+            // SKIP WHEN IT ENDS WITH "~"
+            // Specify what is done when a file is renamed.
+            if (e.OldName + "~" != e.Name) {
+                // Get the rule name strings
+                string[] oldSplits = e.OldName.Split('\\');
+                string[] newSplits = e.Name.Split('\\');
 
-            // If the length is 1, we're in the main assembly, otherwise this gets handled differently
-            if (oldSplits.Length == 1) {
-                string oldName = e.OldName.Split('.')[0];
-                string newName = e.Name.Split('.')[0];
-            } else {
                 string assemblyName = oldSplits[oldSplits.Length - 2];
                 string oldName = oldSplits[oldSplits.Length - 1].Split('.')[0];
                 string newName = newSplits[newSplits.Length - 1].Split('.')[0];
+
+                dynamic doc = nameDocDict[assemblyName];
+
+                // Check and make sure the old rule actually exists
+                dynamic rule = prog.iLogicAuto.GetRule(doc, oldName);
+
+                if (rule != null) {
+                    Console.WriteLine("Renaming Rule {0} To {1}...", oldName, newName);
+                    string ruleText = rule.text;
+                    prog.iLogicAuto.DeleteRule(doc, oldName);
+                    dynamic newRule = prog.iLogicAuto.AddRule(doc, newName, "");
+                    newRule.text = ruleText;
+                } else {
+                    Console.WriteLine("**OLD RULE {0} DOES NOT EXIST**", oldName);
+                }
+            } else {
+                Console.WriteLine("**VIM SWAP FILE, NOT A RENAME**");
             }
-
-            // SKIP WHEN IT ENDS WITH "~"
-            // Specify what is done when a file is renamed.
-            //if (e.OldName + "~" != e.Name) {
-            //    // Get the rule name strings
-            //    string oldRuleName = e.OldName.Split('.')[0];
-            //    string newRuleName = e.Name.Split('.')[0];
-
-            //    // Check and make sure the old rule actually exists
-            //    dynamic rule = prog.iLogicAuto.GetRule(prog.activeDoc, oldRuleName);
-
-            //    if (rule != null) {
-            //        Console.WriteLine("Renaming Rule {0} To {1}...", oldRuleName, newRuleName);
-            //        string ruleText = rule.text;
-            //        prog.iLogicAuto.DeleteRule(prog.activeDoc, oldRuleName);
-            //        dynamic newRule = prog.iLogicAuto.AddRule(prog.activeDoc, newRuleName, "");
-            //        newRule.text = ruleText;
-            //    } else {
-            //        Console.WriteLine("**OLD RULE {0} DOES NOT EXIST**", oldRuleName);
-            //    }
-            //} else {
-            //    Console.WriteLine("**VIM SWAP FILE, NOT A RENAME**");
-            //}
         }
     }
 }
